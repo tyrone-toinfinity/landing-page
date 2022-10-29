ab6697ca13c6f5337928ccc540754b733c437e2f
Tyrone Woody <bobbywoody7@gmail.com>
2022-10-29 15:24:53 -0600

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,77 @@
 import { Footer } from "./components/Footer";
+import { BsHandbag } from "react-icons/bs";
 
 function App() {
   return (
     <div>
       <div className="min-h-screen flex flex-col text-white">
-        <main className="container mx-auto px-6 pt-16 flex-1 text-center"></main>
+        {/* navbar */}
+        <header className="bg-white text-black py-5">
+          <nav className="flex justify-between mx-10">
+            <ul className="flex items-center ">
+              <li className="flex justify-between mr-5 cursor-pointer">
+                {" "}
+                <a>
+                  <img
+                    src="./logos/logo.svg"
+                    width={50}
+                    alt="vr logo"
+                    // className="mr-1 "
+                  />{" "}
+                </a>
+                {/* <a href="#" className="mx-3 hover:opacity-50 duration-150"> */}
+                {/* <p className="text-lg">seeVR</p> */}
+                {/* </a> */}
+              </li>
+              <li>
+                {" "}
+                <a href="#" className="mx-3 hover:opacity-50 duration-150">
+                  Shop
+                </a>
+              </li>
+              <li>
+                {" "}
+                <a href="#" className="mx-3 hover:opacity-50 duration-150">
+                  Our Technologies
+                </a>
+              </li>
+              <li>
+                {" "}
+                <a href="#" className="mx-3 hover:opacity-50 duration-150">
+                  Build With Us
+                </a>
+              </li>
+            </ul>
+            <ul className="flex justify-between items-center">
+              <li>
+                {" "}
+                <a href="#" className="mx-3 hover:opacity-50 duration-150">
+                  Support
+                </a>
+              </li>
+              <li>
+                <BsHandbag className=" hover:text-blue-900 text-lg cursor-pointer ml-4" />
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="container mx-auto px-6 pt-16 flex-1 text-center grid gap-4 grid-cols-3 grid-rows-3">
+          <div className="line"></div>
+          <div className="col-start-3 row-start-2 self-center ">
+            {" "}
+            <div>
+              {" "}
+              <h1 className="font-bold text-6xl tracking-wide uppercase product-title">
+                Meet quill pro
+              </h1>
+              <p className="mt-5 text-lg lowercase product-desc">
+                Unlock new prespectives into how you work, create and
+                collaborate.
+              </p>
+            </div>
+          </div>
+        </main>
+
         <Footer />
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-
+import { BsTwitter, BsFacebook, BsInstagram } from "react-icons/bs";
 export const Footer = () => {
   return (
-    <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between ">
-      <div className=" flex flex-row">
-        <img src="./logos/logo.svg" width={50} alt="vr logo" className="mr-2" />{" "}
-        <p>seeVR</p>
+    <footer className="container mx-auto p-5 flex flex-col md:flex-row items-center justify-between bg-white text-black">
+      <div className=" flex flex-row text-blue-400 gap-x-7 text-xl">
+        <BsTwitter className="hover:opacity-60 cursor-pointer" />
+        <BsFacebook className="hover:opacity-60 cursor-pointer" />
+        <BsInstagram className="hover:opacity-60 cursor-pointer" />
       </div>
 
       <div className="flex -mx-6">
-        <a href="#" className="mx-3 hover:opacity-80 duration-150">
+        <a href="#" className="mx-3 hover:opacity-50 duration-150">
           About us
         </a>
-        <a href="#" className="mx-3 hover:opacity-80 duration-150">
+        <a href="#" className="mx-3 hover:opacity-50 duration-150">
           Privacy
         </a>
-        <a href="#" className="mx-3 hover:opacity-80 duration-150">
+        <a href="#" className="mx-3 hover:opacity-50 duration-150">
           Contact
         </a>
       </div>
-      <div></div>
     </footer>
   );
 };
